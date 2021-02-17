95a9dcf082358569d4652fd0c91d3b0b699445ac
Danielle Koduru <69174710+DanielleKoduru@users.noreply.github.com>
2021-02-16 20:54:56 -0700

    server, index, middleware done

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,50 @@
-function logger(req, res, next) {
-  // DO YOUR MAGIC
+const users = require("../users/users-model")
+// const posts = require("../posts/posts-model")
+
+function logger() {
+  return (req, res, next) => {
+    const time = new Date().toISOString()
+    console.log(`${time} ${req.method} ${req.url}`)
+    next()
+  }
 }
 
-function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
+function validateUserId() {
+  return (req, res, next) => {
+    users.findById(req.params.id)
+      .then((user) => {
+        if (user) {
+          req.user = user
+          next()
+        } else {
+          res.status(404).json({
+            message: "User not found",
+          })
+        }
+      })
+  }
 }
 
-function validateUser(req, res, next) {
-  // DO YOUR MAGIC
+function validateUser() {
+  return (req, res, next) => {
+    if (!req.body) {
+      return res.status(400).json({ message: "missing user data" })
+    } else if (!req.body.text) {
+      return res.status(400).json({ message: "missing required name field" })
+    }
+    next()
+  }
 }
 
-function validatePost(req, res, next) {
-  // DO YOUR MAGIC
+function validatePost() {
+  return (req, res, next) => {
+    if (!req.body) {
+      return res.status(400).json({ message: "missing post data" })
+    } else if (!req.body.text) {
+      return res.status(400).json({ message: "missing required text field" })
+    }
+    next()
+  }
 }
 
-// do not forget to expose these functions to other modules
+module.exports = { logger, validateUserId, validateUser, validatePost }
