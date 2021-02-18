b6bf5a2051abd3ba52ab14b2990d621c7c577bc7
Danielle Koduru <69174710+DanielleKoduru@users.noreply.github.com>
2021-02-18 14:56:39 -0700

    MVP

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,17 +1,34 @@
 const express = require('express');
 const posts = require("./posts-model")
-const { validateUserId, validateUser } = require("../middleware/middleware")
-
+const { validateUserId, validateUser, validatePost } = require("../middleware/middleware")
+const { post } = require('../users/users-router');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  // DO YOUR MAGIC
+router.get('/', (req, res, next) => {
+  const options = {
+    sortBy: req.query.sortby,
+    limit: req.query.limit
+  }
+  posts.get(options)
+    .then((users) => {
+      res.status(200).json(users)
+      next()
+    })
+    .catch((err) => {
+      next(err)
+    })
 });
 
-router.get('/:id', (req, res) => {
-  // DO YOUR MAGIC
+router.get('/:id', (req, res, next) => {
+  post.getByID(req.params.id)
+  .then((posts) => {
+    res.status(200).json(posts)
+    next()
+  })
+  .catch((err) => {
+    next(err)
+  })
 });
 
-// do not forget to export the router
 module.exports = router
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,43 +1,87 @@
 const express = require('express');
-const { validatePost } = require("../middleware/middleware")
+const users = require("./users-model")
+const posts = require("../posts/posts-model")
+// const {userParams, whereNotExists } = require("../../data/db-config")
+const { validateUserId, validateUser, validatePost } = require("../middleware/middleware")
 
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/users', (req, res, next) => {
   // RETURN AN ARRAY WITH ALL THE USERS
+  const options = {
+    sortBy: req.query.sortby,
+    limit: req.query.limit
+  }
+  users.get(options)
+  .then((users) => {
+    res.status(200).json(users)
+    next()
+  })
+  .catch(next) 
 });
 
-router.get('/:id', (req, res) => {
+router.get('/users/:id', validateUserId(), (req, res) => {
   // RETURN THE USER OBJECT
   // this needs a middleware to verify user id
+  res.status(200).json(req.user)
 });
 
-router.post('/', (req, res) => {
+router.post('/users', validateUser(), (req, res, next) => {
   // RETURN THE NEWLY CREATED USER OBJECT
   // this needs a middleware to check that the request body is valid
+  users.insert(req.body)
+    .then((user) => {
+      res.status(200).json(user)
+    })
+    .catch(next) 
 });
 
-router.put('/:id', (req, res) => {
+router.put('/user/:id', validateUser(), validateUserId(), (req, res, next) => {
   // RETURN THE FRESHLY UPDATED USER OBJECT
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
+  users.update(req.params.id, req.body)
+  .then((user) => {
+    res.status(200).json(user)
+  })
+  .catch(next)
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/users/:id', validateUserId(), (req, res, next) => {
   // RETURN THE FRESHLY DELETED USER OBJECT
   // this needs a middleware to verify user id
+  users.remove(req.params.id)
+    .then((user) => {
+      res.status(200).json(user)
+    })
+    .catch(next)
 });
 
-router.get('/:id/posts', (req, res) => {
+router.get('/:id/posts', validateUserId(), (req, res, next) => {
   // RETURN THE ARRAY OF USER POSTS
   // this needs a middleware to verify user id
+  posts.getUserPosts(req.params.id)
+    .then((post) => {
+      if (post){res.status(200).json(post)
+      } else {
+        res.status(404).json({message:"user has no posts"})
+      }
+    })
+    .catch(next)
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post('/users/:id/posts', validateUserId(), validatePost(), (req, res) => {
   // RETURN THE NEWLY CREATED USER POST
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
+  posts.insert(req.body)
+    .then((post) => {
+      res.status(200).json(post)
+    })
+    .catch(() => {
+      res.status(500).json({message: "Server Error"})
+    })
 });
 
 // do not forget to export the router
